2da566869a5a2a1a9b38849d323f831587b50423
armando-v <>
2023-08-26 22:58:01 -0400

    Update utilization

--- actual_encouters_missing_processing.R
+++ actual_encouters_missing_processing.R
@@ -5,6 +5,11 @@ library(lubridate)
 library(odbc)
 library(glue)
 
+timeOptionsHr_filter <- c("07:00","08:00","09:00", "10:00","11:00","12:00","13:00",
+                          "14:00","15:00","16:00","17:00","18:00","19:00",
+                          "20:00") ## Time Range by Hour Filter
+
+
 conn <- dbConnect(odbc(), "OAO Cloud DB")
 utilization_tbl <- tbl(conn, "utilization_table_new")
 
